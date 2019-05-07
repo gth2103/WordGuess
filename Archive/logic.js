fc00e3ca12b0a9aaceeaa3d74bb6eb0875b3caf5
Grant <gth2103@columbia.edu>
2019-05-07 15:39:26 -0400

    passed tests (penultimate two tests failed, alternative testing passed), added banner image, and began index.html

--- Archive/logic.js
+++ Archive/logic.js
@@ -10,13 +10,12 @@ var getRandomInt = function(max) {
 }
 
 var isCorrectGuess = function(string,  char) {
-    var containsChar = false;
     for(var i = 0; i < string.length; i++) {
         if(string[i] === char) {
-           containsChar = true;
+           return true;
         }
     }
-    return containsChar; 
+    return false; 
 }
 
 var getBlanks = function(string) {
@@ -45,4 +44,69 @@ var getIndices = function(string, char) {
     return indices;
 }
 
+var setupRound = function(string) {
+    var round = {
+        word : string,
+        guessesLeft : 9,
+        wrongGuesses : [],
+        puzzleState : getBlanks(string)
+    }
+    return round;
+}
 
+var updateRound = function(round, char) {
+    if(!(isCorrectGuess(round.word, char) || isCorrectGuess(round.puzzleState, char) || isCorrectGuess(round.wrongGuesses, char))){
+        round.guessesLeft--;
+        round.wrongGuesses.push(char);
+    }
+    else if (isCorrectGuess(round.word, char) && !isCorrectGuess(round.puzzleState, char)) {
+        round.puzzleState = fillBlanks(round.word, round.puzzleState, char);
+    }
+}
+
+var hasWon = function(array) {
+    for(var i = 0; i < array.length; i++) {
+        if(array[i] === "_") {
+            return false;
+        }
+    }
+    return true;
+}
+
+var hasLost = function(integer) {
+    if(integer === 0) {
+        return true;
+    }
+    return false;
+}
+
+var isEndOfRound = function(round) {
+    if(hasWon(round.puzzleState) || hasLost(round.guessesLeft)) {
+        return true;
+    }
+    return false;
+}
+
+var setupGame = function(array, integerW, integerL) {
+    var game = {
+        words: array,
+        wins: integerW,
+        losses: integerL,
+        round: setupRound(randomWord(array))
+    }
+    return game;
+}
+
+var startNewRound = function(game){
+    if(hasWon(game.round.puzzleState)){
+        alert("Congratulations, you won! The correct word was '" + game.round.word + "'. Play again!");
+        game.wins++;
+    }
+    else if(hasLost(game.round.guessesLeft)){
+        alert("Sorry, you lost! The correct word was '" + game.round.word + "'. Try again!");
+        game.losses++;
+    }
+    game.round  = setupRound(randomWord(array));
+}
+
+var myGame = setupGame(gameWords, 0, 0);
